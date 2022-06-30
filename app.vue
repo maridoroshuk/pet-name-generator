d2d4f72f5c3bf6329b25de04b8b1a27fd0927646
maridoroshuk <m.doroshuk@bk.ru>
2022-06-30 21:17:21 +0300

    feat: added logic for selecting names

--- app.vue
+++ app.vue
@@ -1,20 +1,5 @@
 <script setup lang="ts">
-  enum Gender {
-    GIRL = "Girl",
-    BOY = "Boy",
-    UNISEX = "Unisex"
-  }
-
-  enum Popularity {
-    TRENDY = "Trendy",
-    UNIQUE = "Unique",
-  }
-
-  enum Length {
-    LONG = "Long",
-    ALL = "All",
-    SHORT = "Short"
-  }
+  import { Gender, Popularity, Length, names } from "@/data"
 
   interface OptionsState {
     gender: Gender;
@@ -27,6 +12,22 @@
     popularity: Popularity.UNIQUE,
     length: Length.ALL
   })
+
+  const computeSelectedNames = () => {
+    const filteredNames = names
+    .filter((name) => name.gender === options.gender)
+    .filter((name) => name.popularity === options.popularity)
+    .filter((name) => {
+      if(options.length === Length.ALL) return true
+      else return name.length === options.length
+    })
+
+    selectedNames.value = filteredNames.map(name => name.name)
+  }
+
+  const selectedNames = ref<string[]>([])
+
+
 </script>
 
 <template>
@@ -40,18 +41,21 @@
 					<button 
           class="option option-left"
           :class="options.gender === Gender.BOY && 'option-active'"
+          @click="options.gender = Gender.BOY"
           >
           Boy
           </button>
 					<button 
           class="option"
           :class="options.gender === Gender.UNISEX && 'option-active'"
+          @click="options.gender = Gender.UNISEX"
           >
           Unisex
           </button>
 					<button 
           class="option option-right"
           :class="options.gender === Gender.GIRL && 'option-active'"
+          @click="options.gender = Gender.GIRL"
           >
           Girl
           </button>
@@ -63,12 +67,14 @@
 					<button 
           class="option option-left"
           :class="options.popularity === Popularity.TRENDY && 'option-active'"
+          @click="options.popularity = Popularity.TRENDY"
           >
           Trendy
           </button>
 					<button 
           class="option option-right"
           :class="options.popularity === Popularity.UNIQUE && 'option-active'"
+          @click="options.popularity = Popularity.UNIQUE"
           >
           Unique
           </button>
@@ -80,24 +86,39 @@
 					<button 
           class="option option-left"
           :class="options.length === Length.LONG && 'option-active'"
+          @click="options.length = Length.LONG"
           >
           Long
           </button>
 					<button 
           class="option"
           :class="options.length === Length.ALL && 'option-active'"
+          @click="options.length = Length.ALL"
           >
           All
           </button>
 					<button 
           class="option option-right"
           :class="options.length === Length.SHORT && 'option-active'"
+          @click="options.length = Length.SHORT"
           >
           Short
           </button>
 				</div>
       </div>
+      <button 
+      class="primary"
+      @click="computeSelectedNames"
+      >
+      Find Names
+      </button>
 		</div>
+    <div class="cards-container">
+      <div v-for="name in selectedNames" :key="name" class="card">
+        <h4>{{name}}</h4>
+        <p>x</p>
+      </div>
+    </div>
 	</div>
 </template>
 
@@ -151,6 +172,42 @@
   .option-active {
     background-color: rgb(249, 87, 89);
     color: white;
+  }
+
+  .primary {
+    background-color: rgb(249, 87, 89);
+    color: white;
+    border-radius: 6.5rem;
+    border: none;
+    padding: 0.75rem 4rem;
+    font-size: 1rem;
+    margin-top: 1rem;
+    cursor: pointer;
+  }
+
+  .cards-container {
+    display: flex;
+    margin-top: 3rem;
+    flex-wrap: wrap;
+  }
+
+  .card {
+    background-color: rgb(27, 60, 138);
+    width: 28%;
+    color: white;
+    border-radius: 1rem;
+    padding: 0.1rem;
+    margin-right: 0.5rem;
+    margin-bottom: 1rem;
+    position: relative;
+  }
+
+  .card p {
+    position: absolute;
+    top: -29%;
+    left: 92.5%;
+    cursor: pointer;
+    color: rgba(255, 255, 255, 0.178);
   }
 </style>
 
